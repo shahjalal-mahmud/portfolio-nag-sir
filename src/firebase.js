5214490f622b6ca04bb6adfc14f061db942b1c98
shahjalal-mahmud <mahmud.nubtk@gmail.com>
2025-07-24 01:12:47 +0600

    Added env and example env

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBcb8IwbNZ40bWh2q2GPC3PwJMwQ1jqwoU",
-  authDomain: "portfolio-nag-sir.firebaseapp.com",
-  projectId: "portfolio-nag-sir",
-  storageBucket: "portfolio-nag-sir.firebasestorage.app",
-  messagingSenderId: "716075794237",
-  appId: "1:716075794237:web:d2504f9a7257e608888f54",
-  measurementId: "G-GFZR9V30XY"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
