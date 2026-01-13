bef3f61fd7f760dc172c54f5d959748b6cca75c2
shahjalal-mahmud <mahmud.nubtk@gmail.com>
2026-01-13 21:54:40 +0600

    Update the Tailwind v4 from v3

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // [!code ++]
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react()
+    react(),
+    tailwindcss(), // [!code ++]
   ],
 })
